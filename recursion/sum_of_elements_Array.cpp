c546e01314d4d23396a3b21281446053bf039e0e
Reemaa828 <reemoto51@gmail.com>
2024-04-26 22:14:53 +0300

    new

---
+++ recursion/sum_of_elements_Array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int summ_of_elements(int arr[],int size){
+    if (size==0){
+        return 0;
+    }
+    return arr[size-1]+summ_of_elements(arr,size-1);
+}
+int main(){
+    int arr[]={1,2,3,4,5};
+    cout<<summ_of_elements(arr,5);
+}
