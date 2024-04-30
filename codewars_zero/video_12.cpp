39f3180c6439ee22b07efeb4e5be432965828024
Reemaa828 <reemoto51@gmail.com>
2024-04-30 03:17:00 +0300

    new

--- codewars_zero/video_12.cpp
+++ codewars_zero/video_12.cpp
@@ -19,32 +19,32 @@ string rock_paper_scissor(string player1, string player2){
     string no="draw";
     if(player1==player2)
     {
-        msg+=no;
+        msg=no;
     }
     if(player1=="paper"){
         if(player2=="rocks"){
-            msg+=p1;
+            msg=p1;
         }
         else if(player2=="scissor"){
-            msg+=p2;
+            msg=p2;
         }
     }
     
     if(player1=="rocks"){
         if(player2=="scissor"){
-            msg+=p1;
+            msg=p1;
         }
         else if(player2=="paper"){
-            msg+=p2;
+            msg=p2;
         }
     }
     
     if(player1=="scissor"){
         if(player2=="paper"){
-            msg+=p1;
+            msg=p1;
         }
         else if(player2=="rocks"){
-            msg+=p2;
+            msg=p2;
         }
     }
     return msg;
@@ -54,6 +54,6 @@ string rock_paper_scissor(string player1, string player2){
 int main(){
 
     //cout<<correct_text("remm5ahsh1ndnms0df");
-    //cout<<rock_paper_scissor("rocks","scissor");
+    cout<<rock_paper_scissor("rocks","scissor");
 
 }
